3e1c592438151b862205ee99e118d6eb9f47255f
IGSON2 <suninnaa@gmail.com>
2023-06-22 23:03:14 +0900

    change deploy.yml

--- backend/utilities/random_test.go
+++ backend/utilities/random_test.go
@@ -1,6 +1,11 @@
 package utilities
 
-import "testing"
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRandomFloat(t *testing.T) {
 	t.Log(MakeRanFloat(20000, 30000))
@@ -8,4 +13,15 @@ func TestRandomFloat(t *testing.T) {
 
 func TestRandomInt(t *testing.T) {
 	t.Log(MakeRanInt(10, 100))
+}
+
+func TestMakeRanKey(t *testing.T) {
+	var sb strings.Builder
+	alphaNum := "abcdefghijklmnopqrstuvwxyz0123456789"
+	for i := 0; i < 32; i++ {
+		c := alphaNum[MakeRanInt(0, len(alphaNum))]
+		sb.WriteByte(c)
+	}
+	t.Log(sb.String())
+	require.Equal(t, 32, len([]byte(sb.String())))
 }
