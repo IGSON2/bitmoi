562f9bda11ad923f31e78ddd568c0e9f89f084c5
Igson <91243660+IGSON2@users.noreply.github.com>
2023-07-11 16:21:54 +0900

    Update moi token contract

--- frontend/src/contract/moi.sol
+++ frontend/src/contract/moi.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.1;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "../../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract MoiToken is Ownable, ERC20 {
     bool public limited;
@@ -15,13 +15,15 @@ contract MoiToken is Ownable, ERC20 {
     mapping(string => bool) public adLocations;
     string[] public locationKeys;
 
-    mapping(string => mapping(address => uint256)) private locks;
-    mapping(string => address[]) private lockAddress;
+    mapping(string => mapping(address => uint256)) public locks;
+    mapping(string => address[]) public lockAddress;
 
-    struct sortedAddrBalance {
+    struct addrAmount {
         address addr;
         uint256 amount;
     }
+
+    mapping(string => addrAmount) public highestbidder;
 
     event TokensLocked(
         address indexed account,
@@ -35,6 +37,7 @@ contract MoiToken is Ownable, ERC20 {
     );
     event LocationAdded(string location);
     event LocationRemoved(string location);
+    event AdFeePaid(string location, address _addr, uint256 amount);
 
     constructor(uint256 _totalSupply) ERC20("Bitmoi", "MOI") {
         _mint(msg.sender, _totalSupply);
@@ -66,11 +69,32 @@ contract MoiToken is Ownable, ERC20 {
     function removeAdLocation(
         string memory _adLocation
     ) external onlyOwner mustExistLoc(_adLocation) {
-        delete adLocations[_adLocation];
+        for (uint256 i = 0; i < lockAddress[_adLocation].length; i++) {
+            address _addr = lockAddress[_adLocation][i];
+            uint256 _amount = locks[_adLocation][_addr];
+            if (_amount > 0) {
+                _transfer(address(this), _addr, _amount);
+                emit TokensUnlocked(_addr, _amount, _adLocation);
+                delete locks[_adLocation][_addr];
+            }
+        }
 
+        delete adLocations[_adLocation];
+        delete lockAddress[_adLocation];
+        delete highestbidder[_adLocation];
+
+        uint256 locationKeysLength = locationKeys.length;
         uint256 idx = findLocationIdx(_adLocation);
-        require(idx > 0, "Invalid location for location key array");
-        delete locationKeys[idx];
+
+        string[] memory newLocationKeys = new string[](locationKeysLength - 1);
+
+        for (uint256 i = 0; i < idx; i++) {
+            newLocationKeys[i] = locationKeys[i];
+        }
+        for (uint256 i = idx + 1; i < locationKeysLength; i++) {
+            newLocationKeys[i - 1] = locationKeys[i];
+        }
+        locationKeys = newLocationKeys;
 
         emit LocationRemoved(_adLocation);
     }
@@ -99,9 +123,15 @@ contract MoiToken is Ownable, ERC20 {
     ) external onlyOwner mustExistLoc(_adLocation) moreThanZeroAmt(_amount) {
         _transfer(_from, address(this), _amount);
 
+        if (locks[_adLocation][_from] == 0) {
+            lockAddress[_adLocation].push(_from);
+        }
+
         locks[_adLocation][_from] += _amount;
 
-        lockAddress[_adLocation].push(_from);
+        if (locks[_adLocation][_from] > highestbidder[_adLocation].amount) {
+            highestbidder[_adLocation] = addrAmount(_from, _amount);
+        }
 
         emit TokensLocked(_from, _amount, _adLocation);
     }
@@ -109,6 +139,7 @@ contract MoiToken is Ownable, ERC20 {
     function unlockTokens() external onlyOwner {
         for (uint256 i = 0; i < locationKeys.length; i++) {
             string memory loc = locationKeys[i];
+            payAdFee(loc);
             for (uint256 j = 0; j < lockAddress[loc].length; j++) {
                 address addr = lockAddress[loc][j];
                 uint256 transferAmt = locks[loc][addr];
@@ -120,8 +151,24 @@ contract MoiToken is Ownable, ERC20 {
                 emit TokensUnlocked(addr, transferAmt, loc);
                 delete locks[loc][addr];
             }
-            delete locationKeys[i];
+            delete lockAddress[loc];
         }
+    }
+
+    function payAdFee(
+        string memory _adLocation
+    ) public onlyOwner mustExistLoc(_adLocation) {
+        delete locks[_adLocation][highestbidder[_adLocation].addr];
+        emit AdFeePaid(
+            _adLocation,
+            highestbidder[_adLocation].addr,
+            highestbidder[_adLocation].amount
+        );
+        delete highestbidder[_adLocation];
+    }
+
+    function withdraw() public onlyOwner {
+        _transfer(address(this), owner(), balanceOf(address(this)));
     }
 
     function getLockedTokens(
@@ -129,7 +176,6 @@ contract MoiToken is Ownable, ERC20 {
         string memory _adLocation
     ) public view returns (uint256) {
         uint256 lockedAmount = locks[_adLocation][_from];
-
         return lockedAmount;
     }
 
@@ -146,33 +192,6 @@ contract MoiToken is Ownable, ERC20 {
             }
         }
         return addrCnt;
-    }
-
-    function sortByLocation(
-        string memory _adLocation
-    ) public view returns (sortedAddrBalance[] memory) {
-        sortedAddrBalance[] memory sortedAddr;
-
-        for (uint256 i = 0; i < lockAddress[_adLocation].length; i++) {
-            address _addr = lockAddress[_adLocation][i];
-            uint256 _amount = locks[_adLocation][_addr];
-            for (uint256 j = 0; j < sortedAddr.length; j++) {
-                if (_amount > sortedAddr[j].amount) {
-                    (sortedAddr[j], sortedAddr[j + 1]) = (
-                        sortedAddrBalance(_addr, _amount),
-                        sortedAddr[j]
-                    );
-                } else if (_amount == sortedAddr[j].amount) {
-                    (sortedAddr[j + 1], sortedAddr[j]) = (
-                        sortedAddr[j],
-                        sortedAddrBalance(_addr, _amount)
-                    ); // 먼저 예치한 사람이 우위
-                }
-                sortedAddr[0] = (sortedAddrBalance(_addr, _amount));
-            }
-        }
-
-        return sortedAddr;
     }
 
     function blacklist(
