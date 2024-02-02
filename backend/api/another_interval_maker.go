ef7f7ad21adff6c213119a7103d1bf9a6d417309
IanJo-Rigo <91243660+IGSON2@users.noreply.github.com>
2024-02-02 11:30:20 +0900

    dprecated exceed chart logic

--- backend/api/another_interval_maker.go
+++ backend/api/another_interval_maker.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	db "bitmoi/backend/db/sqlc"
 	"bitmoi/backend/utilities"
 	"bitmoi/backend/utilities/common"
 	"encoding/json"
@@ -28,11 +27,6 @@ func (s *Server) sendAnotherInterval(a *AnotherIntervalRequest, c *fiber.Ctx) (*
 			originInfo.Name, a.ReqInterval, originInfo.RefTimestamp, err)
 	}
 
-	err = s.cutExceedChart(originInfo.Name, originInfo.RefTimestamp, c, cdd)
-	if err != nil {
-		return nil, fmt.Errorf("cannot cut exceed chart. name : %s, interval : %s, err : %w", originInfo.Name, a.ReqInterval, err)
-	}
-
 	var oc = &OnePairChart{
 		Name:         originInfo.Name,
 		OneChart:     cdd,
@@ -52,22 +46,4 @@ func (s *Server) sendAnotherInterval(a *AnotherIntervalRequest, c *fiber.Ctx) (*
 		oc.EntryPrice = oc.OneChart.PData[0].Close
 	}
 	return oc, nil
-}
-
-// 최초 전송 차트의 interval이 1h이므로 이보다 작은 단위의 another interval 요청은
-// reqTimestamp에 더 가까이 접근하여 entrytime보다 미래의 캔들을 불러온다. cutExceedChart은 이를 방지한다.
-func (s *Server) cutExceedChart(name string, originTimestamp int64, c *fiber.Ctx, cd *CandleData) error {
-	var cuttingCnt int
-	pdatas := cd.PData
-	entryTIme, err := s.store.Get1hEntryTimestamp(c.Context(), db.Get1hEntryTimestampParams{Name: name, Time: originTimestamp})
-	if err != nil {
-		return err
-	}
-
-	for i := 0; pdatas[i].Time > entryTIme; i++ {
-		cuttingCnt++
-	}
-	cd.PData = cd.PData[cuttingCnt:]
-	cd.VData = cd.VData[cuttingCnt:]
-	return nil
 }
